d0881c34a36a39eaa9923e2776776357d430ba31
Hanno Pleis <hanno@ulfertsprygoda.de>
2022-09-01 11:16:42 +0200

    add rotate loop

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const loehr = document.querySelector(".img");
 let angle = 0;
+let intId;
 
 const rotate = () => {
-    angle += 45;
+    angle += 10;
     loehr.style.transform = `rotate(${angle}deg)`;        
     
 }
 
-loehr.addEventListener("mousedown", rotate)
+const rotateLoop = () => {
+    intId = setInterval (()=> rotate(), 20)
+    console.log(intId)
+}
+
+loehr.addEventListener("mouseenter", rotateLoop)
+loehr.addEventListener("mouseleave", () => {clearInterval(intId)})
